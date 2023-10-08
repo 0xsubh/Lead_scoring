026e202e1439663e7ddf5bb4696126760861eb33
Subham Sharma <subham123sharma.jhar123@gmail.com>
2023-10-08 19:03:51 +0530

    main
    added hii

--- leadscoring.py
+++ leadscoring.py
@@ -6,6 +6,7 @@ import plotly.express as px
 import plotly.graph_objects as go
 import warnings
 print("hello")
+print("hii")
 from scipy.stats import linregress, uniform
 from sklearn.model_selection import train_test_split, cross_val_score, StratifiedKFold, RandomizedSearchCV
 from sklearn.compose import make_column_transformer, make_column_selector
